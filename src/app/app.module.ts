4dff1ef8d35764febacd475872a4f3e1ac85f728
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-17 21:56:19 +0200

    fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from '../components/navbar/navbar.component';
-import { ContactManagerComponent } from '../components/contact-manager/contact-manager.component';
-import { EditComponent } from '../components/edit/edit.component';
-import { ViewContactComponent } from '../components/view-contact/view-contact.component';
-import { SpinnerComponent } from '../components/spinner/spinner.component';
-import { AddContactComponent } from '../components/add-contact/add-contact.component';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
 @NgModule({
   declarations: [
     AppComponent,
